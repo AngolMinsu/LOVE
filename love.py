c8c4a3eab617cc862d31b2c32a84afcfcb59353d
AngolMinsu <steinsgate888@gmail.com>
2024-05-20 12:55:32 +0900

    hiragana

--- love.py
+++ love.py
@@ -4,21 +4,24 @@ import string
 RED = "\033[91m"
 RESET = "\033[0m"
 
-def generate_random_uppercase():
-    return random.choice(string.ascii_uppercase)
+def generate_hiragana():
+    start = 0x3040
+    end = 0x309F
+    hiragana = random.choice(range(start,end))
+    return chr(hiragana)
 
 def main():
     MIKADO = []
     while True:
-        char = generate_random_uppercase()
+        char = generate_hiragana()
         print(char, end=" ")
         MIKADO.append(char)
         
-        if len(MIKADO) > 6:
+        if len(MIKADO) > 3:
             MIKADO.pop(0)
         
-        if ''.join(MIKADO[-6:]) == "MIKADO":
-            print("\b"*12 + RED + "M I K A D O" + RESET)
+        if ''.join(MIKADO[-3:]) == "みかど":
+            print("\b"*6 + RED + "み か ど" + RESET)
             break
 
 if __name__ == "__main__":
